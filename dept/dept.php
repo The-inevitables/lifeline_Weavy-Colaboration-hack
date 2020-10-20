b4dea01781cadfe2b14f09ba314d0a827a71a943
Akshay V Anil <30944903+akshay117@users.noreply.github.com>
2020-10-21 00:44:07 +0530

    Add files via upload

---
+++ dept/dept.php
@@ -0,0 +1,225 @@
+<!DOCTYPE html>
+<html lang="en">
+   <!-- Basic -->
+   <meta charset="utf-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <!-- Mobile Metas -->
+   <meta name="viewport" content="width=device-width, initial-scale=1">
+   <meta name="viewport" content="initial-scale=1, maximum-scale=1">
+   <!-- Site Metas -->
+   <title>Life Line</title>
+   <meta name="keywords" content="">
+   <meta name="description" content="">
+   <meta name="author" content="">
+   <!-- Site Icons -->
+   <link rel="shortcut icon" href="images/fevicon.ico.png" type="image/x-icon" />
+   <link rel="apple-touch-icon" href="images/apple-touch-icon.png">
+   <!-- Bootstrap CSS -->
+   <link rel="stylesheet" href="css/bootstrap.min.css">
+   <!-- Site CSS -->
+   <link rel="stylesheet" href="style.css">
+   <!-- Colors CSS -->
+   <link rel="stylesheet" href="css/colors.css">
+   <!-- ALL VERSION CSS -->
+   <link rel="stylesheet" href="css/versions.css">
+   <!-- Responsive CSS -->
+   <link rel="stylesheet" href="css/responsive.css">
+   <!-- Custom CSS -->
+   <link rel="stylesheet" href="css/custom.css">
+   <!-- Modernizer for Portfolio -->
+   <script src="js/modernizer.js"></script>
+   <!-- [if lt IE 9] -->
+   <title>LifeLine DEPARTMENTS | Departments :: LifeLine </title>
+	<!--/tags -->
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<meta name="keywords" content="New Clinic Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template, 
+Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyEricsson, Motorola web design" />
+	<script type="application/x-javascript">
+		addEventListener("load", function () {
+			setTimeout(hideURLbar, 0);
+		}, false);
+
+		function hideURLbar() {
+			window.scrollTo(0, 1);
+		}
+	</script>
+	<!--//tags -->
+	<link href="css/bootstrap.css" rel="stylesheet" type="text/css" media="all" />
+	<link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
+	<link href="css/appointment_style.css" rel="stylesheet" type="text/css" media="all" />
+	<link href="css/font-awesome.css" rel="stylesheet">
+	<!-- //for bootstrap working -->
+	<link href="//fonts.googleapis.com/css?family=Source+Sans+Pro:300,300i,400,400i,600,600i,700" rel="stylesheet">
+   </head>
+   <body class="clinic_version">
+      <!-- LOADER -->
+      <div id="preloader">
+         <img class="preloader" src="images/loaders/heart-loading2.gif" alt="">
+      </div>
+      <!-- END LOADER -->
+      <header>
+      <!-- header -->
+	<div class="header" id="home">
+		<div class="top_menu_w3layouts">
+<div class="container">		
+			<div class="clearfix"> </div>
+			</div>
+		</div>
+		<div class="content white">
+			<nav class="navbar navbar-default" role="navigation">
+				<div class="container">
+					<div class="navbar-header">
+						<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+					<span class="sr-only">Toggle navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+						<a class="navbar-brand" href="index.html">
+							<h1><span class="fa fa-stethoscope" aria-hidden="true"></span>LifeLine </h1>
+						</a>
+					</div>
+					<!--/.navbar-header-->
+					<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+					</div>
+					<!--/.navbar-collapse-->
+					<!--/.navbar-->
+				</div>
+			</nav>
+		</div>
+	</div>
+	<!-- banner -->
+	<!--//banner -->
+	<!-- departments -->
+	<div class="services">
+<div class="container">
+		<h3 class="heading-agileinfo">Departments<span>Don’t worry, it’s easy. Believe in us and keep moving.</span></h3>
+	
+		<div class="services-top-grids">
+			<div class="col-md-4">
+				<div class="grid1">
+					<i class="fa fa-heartbeat" aria-hidden="true"></i>
+					<h4>Cardiology</h4>
+                    <h4>Doctor Rejin Joseph</h4>
+					<p></p>
+				</div>
+			</div>
+			<div class="col-md-4">
+				<div class="grid1">
+					<i class="fa fa-user-md" aria-hidden="true"></i>
+					<h4>Traumotology</h4>
+                    <h4>Doctor Akshatha K</h4>
+					<p></p>
+				</div>
+			</div>
+			<div class="col-md-4">
+				<div class="grid1">
+					<i class="fa fa-wheelchair-alt" aria-hidden="true"></i>
+					<h4>For disabled</h4>
+                    <h4>Doctor Asha Sarath</h4>
+					<p> </p>
+				</div>
+			</div>
+			<div class="clearfix"></div>
+		</div>
+		<div class="services-top-grids">
+			<div class="col-md-4">
+				<div class="grid1">
+					<i class="fa fa-medkit" aria-hidden="true"></i>
+					<h4>Pulmonary</h4>
+                    <h4>Doctor Kevin K</h4>
+					<p></p>
+				</div>
+			</div>
+			<div class="col-md-4">
+				<div class="grid1">
+					<i class="fa fa-hospital-o" aria-hidden="true"></i>
+					<h4>Gynaecology</h4>
+                    <h4>Doctor Ajai Manuel</h4>
+					<p></p>
+				</div>
+			</div>
+			<div class="col-md-4">
+				<div class="grid1">
+					<i class="fa fa-ambulance" aria-hidden="true"></i>
+					<h4>Dental</h4>
+                    <h4>Doctor Ashik John</h4>
+					<p></p>
+				</div>
+			</div>
+			<div class="clearfix"></div>
+		</div>
+		<div class="services-bottom-grids">
+			<div class="col-md-4">
+				<div class="grid1">
+					<i class="fa fa-plus-square" aria-hidden="true"></i>
+					<h4>Orthology</h4>
+                    <h4>Doctor Arathy Prathap</h4>
+					<p></p>
+				</div>
+			</div>
+			<div class="col-md-4">
+				<div class="grid1">
+					<i class="fa fa-stethoscope" aria-hidden="true"></i>
+					<h4>Nephrology</h4>
+                    <h4>Doctor Miya George</h4>
+					<p></p>
+				</div>
+			</div>
+			<div class="col-md-4">
+				<div class="grid1">
+					<i class="fa fa-user-md" aria-hidden="true"></i>
+					<h4>Neurology</h4>
+                    <h4>Doctor Akasha Joshy</h4>
+					<p></p>
+				</div>
+			</div>
+			<div class="clearfix"></div>
+		</div>
+	</div>
+</div>
+<!-- //departments -->
+	<!-- footer -->
+
+	<div class="footer_wthree_agile">
+		<p>© 2020 LifeLine.</a></p>
+	</div>
+<!-- //footer -->
+	<!-- bootstrap-modal-pop-up -->
+	<div class="modal video-modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModal">
+		<div class="modal-dialog" role="document">
+			<div class="modal-content">
+				<div class="modal-header">
+					LifeLine
+					<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>						
+				</div>
+					<div class="modal-body">
+						<img src="images/g7.jpg" alt=" " class="img-responsive" />
+					</div>
+			</div>
+		</div>
+	</div>
+<!-- //bootstrap-modal-pop-up --> 
+	<!-- js -->
+	<script type="text/javascript" src="js/jquery-2.1.4.min.js"></script>
+	<script>
+		$('ul.dropdown-menu li').hover(function () {
+			$(this).find('.dropdown-menu').stop(true, true).delay(200).fadeIn(500);
+		}, function () {
+			$(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut(500);
+		});
+	</script>
+	<script type="text/javascript" src="js/bootstrap.js"></script>
+    
+	
+      <!-- end copyrights -->
+      <a href="#home" data-scroll class="dmtop global-radius"><i class="fa fa-angle-up"></i></a>
+      <!-- all js files -->
+      <script src="js/all.js"></script>
+      <!-- all plugins -->
+      <script src="js/custom.js"></script>
+      <!-- map -->
+     <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCNUPWkb4Cjd7Wxo-T4uoUldFjoiUA1fJc&callback=myMap"></script>
+   </body>
+</html>
---
+++ faq/faq.php
@@ -0,0 +1,324 @@
+<!DOCTYPE html>
+<html lang="en">
+   <!-- Basic -->
+   <meta charset="utf-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <!-- Mobile Metas -->
+   <meta name="viewport" content="width=device-width, initial-scale=1">
+   <meta name="viewport" content="initial-scale=1, maximum-scale=1">
+   <!-- Site Metas -->
+   <title>Life Line</title>
+   <meta name="keywords" content="">
+   <meta name="description" content="">
+   <meta name="author" content="">
+   <!-- Site Icons -->
+   <link rel="shortcut icon" href="images/fevicon.ico.png" type="image/x-icon" />
+   <link rel="apple-touch-icon" href="images/apple-touch-icon.png">
+   <!-- Bootstrap CSS -->
+   <link rel="stylesheet" href="css/bootstrap.min.css">
+   <!-- Site CSS -->
+   <link rel="stylesheet" href="style.css">
+   <!-- Colors CSS -->
+   <link rel="stylesheet" href="css/colors.css">
+   <!-- ALL VERSION CSS -->
+   <link rel="stylesheet" href="css/versions.css">
+   <!-- Responsive CSS -->
+   <link rel="stylesheet" href="css/responsive.css">
+   <!-- Custom CSS -->
+   <link rel="stylesheet" href="css/custom.css">
+   <!-- Modernizer for Portfolio -->
+   <script src="js/modernizer.js"></script>
+   <!-- [if lt IE 9] -->
+   </head>
+   <body class="clinic_version">
+      <!-- LOADER -->
+      <div id="preloader">
+         <img class="preloader" src="images/loaders/heart-loading2.gif" alt="">
+      </div>
+      <!-- END LOADER -->
+      <header>
+      
+      <title>COVID awareness site</title>
+    
+    <!-- Favicon -->
+    <link rel="shortcut icon" href="images/favicon/favicon.ico">
+    <link rel="apple-touch-icon" sizes="144x144" type="image/x-icon" href="images/favicon/apple-touch-icon.png">
+    
+    <!-- All CSS Plugins -->
+    <link rel="stylesheet" type="text/css" href="css/plugin.css">
+    
+    <!-- Main CSS Stylesheet -->
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    
+    <!-- Google Web Fonts  -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins:400,300,500,600,700">
+    
+    
+    <!-- HTML5 shiv and Respond.js support IE8 or Older for HTML5 elements and media queries -->
+   
+
+  </head>
+
+ <body>
+
+	
+     
+	 <!-- Preloader Start -->
+     <div class="preloader">
+	   <div class="rounder"></div>
+      </div>
+      <!-- Preloader End -->
+      
+      
+    
+    
+    <div id="main">
+        <div class="container">
+            <div class="row">
+            	
+               
+                 
+                 <!-- About Me (Left Sidebar) Start -->
+                 <div class="col-md-3">
+                   <div class="about-fixed">
+                    
+                     <div class="my-pic">
+                        <img src="images/pic/my-pic.png" alt="">
+                        <a href="javascript:void(0)" class="collapsed" data-target="#menu" data-toggle="collapse"><i class="icon-menu menu"></i></a>
+                         <div id="menu" class="collapse">
+                           <ul class="menu-link">
+                               <li><a href="about.html">About</a></li>
+                               <li><a href="work.html">Work</a></li>
+                               <li><a href="contact.html">Contact</a></li>
+                            </ul>
+                         </div>
+                        </div>
+                      
+                      
+                      
+                      <div class="my-detail">
+                    	
+                        <div class="white-spacing">
+                            <h1>COVID-19</COVID-19></h1>
+                            <span><p>Frequently 
+                              Asked</p> 
+                                      <p>Questions</p></span>
+                        </div> 
+                       
+                       <ul class="social-icon">
+                         <li><a href="#" target="_blank" class="facebook"><i class="fa fa-facebook"></i></a></li>
+                         <li><a href="#" target="_blank" class="twitter"><i class="fa fa-twitter"></i></a></li>
+                         <li><a href="#" target="_blank" class="linkedin"><i class="fa fa-linkedin"></i></a></li>
+                         <li><a href="#" target="_blank" class="github"><i class="fa fa-github"></i></a></li>
+                        </ul>
+
+                    </div>
+                  </div>
+                </div>
+                <!-- About Me (Left Sidebar) End -->
+                
+                
+                
+                
+                 
+                 <!-- Blog Post (Right Sidebar) Start -->
+                 <div class="col-md-9">
+                    <div class="col-md-12 page-body">
+                    	<div class="row">
+                    		
+                            
+                            <div class="sub-title">
+                           		<h2>Frequently Asked Questions</h2>
+                                <a href="contact.html"><i class="icon-envelope"></i></a>
+                             </div>
+                            
+                            
+                            <div class="col-md-12 content-page">
+                            	
+                                
+                                <!-- Blog Post Start -->
+                                <div class="col-md-12 blog-post">
+                                    <div class="post-title">
+                                     <h1>What is the 2019 novel coronavirus, called COVID-19?</h1>
+                                    </div>  
+                                    <div class="post-info">
+                                    	<span>April 22, 2020/ by <a href="#" target="_blank">Rejin joseph</a></span>
+                                    </div>  
+                                    <p>Coronavirus refers to a family of viruses that cause a spectrum of diseases ranging from the common cold to severe life-threatening pneumonia. They’re named for the spike proteins on the surface of the virus that make the virus look like a crown or star.</p>
+                                    <p>The 2019 novel coronavirus was identified as the cause of an outbreak of respiratory illness first detected in Wuhan City, Hubei Province, China, in December 2019. The virus has been named SARS-CoV-2, and the disease it causes has been named coronavirus disease 2019, abbreviated as COVID-19. On March 11, 2020, the COVID-19 outbreak was declared a pandemic by the World Health Organization.</p>
+                                    </div>
+                                <!-- Blog Post End -->
+                                
+                                
+                                
+                                <!-- Blog Post Start -->
+                                <div class="col-md-12 blog-post">
+                                    <div class="post-title">
+                                      <a href="single.html"><h1>How does the virus spread?</h1>
+                                    </div>  
+                                    <div class="post-info">
+                                    	<span>April 22, 2020 / by <a href="#" target="_blank">Rejin Joseph/a></span>
+                                    </div>  
+                                    <p>The virus that causes COVID-19 spreads from person to person. Someone who has COVID-19 can spread the illness to others, even if they don’t show any symptoms. The principle mode of transmission is thought to occur mainly via respiratory droplets that travel up to six feet in the air after an infected person coughs or sneezes. This is similar to how influenza and other viruses spread. These droplets can land in the mouths or noses of people who are nearby or possibly be inhaled into the lungs.</p>
+                                    <p>There is also some evidence that virus particles can remain on contaminated surfaces for several days, but the CDC does not believe that this is the main way that the virus spreads.</p>
+                                    
+                                </div>
+                                <!-- Blog Post End -->
+                                
+                                
+                                
+                                <!-- Blog Post Start -->
+                                <div class="col-md-12 blog-post">
+                                	<div class="post-image">
+                                    	<img src="images/pic/my-pic.png" alt="">                                       
+                                    </div> 
+                                    <div class="post-title">
+                                      <h1>What is viral shedding?</h1>
+                                    </div>  
+                                    <div class="post-info">
+                                    	<span>April 22, 2020 / by <a href="#" target="_blank">Rejin Joseph</a></span>
+                                    </div>  
+                                    <p><em>Updated May 1, 2020</em></p>
+                                    <p>Viral shedding occurs when a virus is detected in body fluids such as nasal secretions and saliva. Evidence suggests that the novel coronavirus is most contagious when symptoms are worse and viral shedding is high. However, it appears that some people can be contagious prior to developing symptoms, suggesting that viral shedding is occurring even early in the infection.</p>
+
+                                </div>
+                                <!-- Blog Post End -->
+                                
+                                
+                                
+                                <!-- Blog Post Start -->
+                                <div class="col-md-12 blog-post">
+                                    <div class="post-title">
+                                      <h1>Is the coronavirus airborne?</h1>
+                                    </div>  
+                                    <div class="post-info">
+                                    	<span>April 22, 2020 / by <a href="#" target="_blank">Akhil M Anil</a></span>
+                                    </div>  
+                                    <p><!--[if !supportLists]--><!--[endif]-->There is limited research about how long the virus stays in the air. The virus is mostly transmitted when people are in close contact with one another, spread by droplets produced when an infected person sneezes or coughs.<p></p></p>
+
+                                </div>
+                                <!-- Blog Post End -->
+                                
+                                
+                                
+                                <!-- Blog Post Start -->
+                                <div class="col-md-12 blog-post">
+                                    <div class="post-title">
+                                      <h1>What should I do if I think I have COVID-19?</h1>
+                                    </div>  
+                                    <div class="post-info">
+                                    	<span>April 22, 2020 / by <a href="#" target="_blank">Arjun Raj</a></span>
+                                    </div>  
+                                                                        <p>If you think you have been exposed to COVID-19 and develop a fever and symptoms such as cough or difficulty breathing, call your health care provider or our patient hotline at 415-514-7328. Please use the UCSF MyChart “Coronavirus &amp; Flu Symptom Checker” before you call the hotline. If you are a patient of the UCSF Cancer Center, please contact that clinic for specific instructions prior to calling the UCSF coronavirus line.</p>
+                                    <p>If you are a UCSF Health patient and have symptoms of COVID-19, we are committed to helping you get tested within 48 hours.</p>
+                                    <p>In the meantime, if you feel sick, take the following precautions:</p>
+                                    <ul><li>Avoid public areas, and stay away from others in your home. If you can use a separate bedroom and bathroom, do so.</li>
+                                      <li>Cover your mouth and nose when coughing or sneezing.</li>
+                                      <li>Wear a mask to cover your nose and mouth if you have cough and are around other people in your home or in public. Please do not wear a mask with a valve, as these masks allow droplet release and do not protect others who may be nearby.</li>
+                                      <li>Wash your hands after touching your face, before eating, after using the bathroom. If soap and water is not available, use hand sanitizer with &gt;60% alcohol.</li>
+                                      <li>Avoid sharing personal household items.</li>
+                                      <li>Clean all high touch services every day such as counters, tabletops, doorknobs, phones, keyboards. Use a household cleaning spray or wipe according to label instructions.</li>
+                                      <li>Monitor your symptoms. If you feel your symptoms are worsening, contact your health care provider.</li>
+                                    </ul><p><a href="https://www.my-emmi.com/eplayer/en/?viewId=DF55B207-EA74-49C8-BE2D-2CDB70F3EB38&amp;video=true&amp;src=emmi&amp;locale=en_US&amp;ccVersion=false" target="_blank">Watch a video that explains more.</a></p>
+                                    <p>If you have a medical emergency, call 911 and notify the dispatch personnel that you have or are being evaluated for COVID-19.</p>
+                                    
+                                </div>
+                                <!-- Blog Post End -->
+                                
+                                
+                                
+                                <!-- Blog Post Start -->
+                                <div class="col-md-12 blog-post">
+                                    <div class="post-title">
+                                      <h1>Should I go to the emergency room if I am sick?</h1>
+                                    </div>  
+                                    <div class="post-info">
+                                    	<span>April 22, 2020/ by <a href="#" target="_blank">Akshay V Anil</a></span>
+                                    </div>  
+                                    <p>If you are sick, please contact your physician or call our patient hotline at 415-514-7328 so that we can best triage your situation.</p>
+                                </div>
+                                <!-- Blog Post End -->
+                                
+                                <div class="col-md-12 text-center">
+                                 <a href="javascript:void(0)" id="load-more-post" class="load-more-button">Load</a>
+                                 <div id="post-end-message"></div>
+                                </div>
+                                
+                             </div>
+                              
+                         </div>
+                           
+                         </div>
+                     
+                     
+                       <!-- Footer Start -->
+                       <div class="col-md-12 page-body margin-top-50 footer">
+                          <footer>
+                          <ul class="menu-link">
+                               <li><a href="http://lifelinecare.epizy.com/?i=1#Ddoctor%20webpage">Home</a></li>
+                               <li><a href="about.html">About</a></li>
+                               <li><a href="work.html">Work</a></li>
+                               <li><a href="contact.html">Contact</a></li>
+                            </ul>
+                            
+                          <p>© Copyright 2020 Team Inevitables. All rights reserved</p>
+						  
+						  
+						  <!-- UiPasta Credit Start -->
+                          <div class="uipasta-credit">Designes By <a href="http://lifelinecare.epizy.com/?i=1#Ddoctor%20webpage" target="_blank">Team Inevitables</a></div>
+                          <!-- UiPasta Credit End -->
+
+                           
+                         </footer>
+                       </div>
+                       <!-- Footer End -->
+                     
+                     
+                  </div>
+                  <!-- Blog Post (Right Sidebar) End -->
+                
+            </div>
+         </div>
+      </div>
+    
+    
+    
+    <!-- Back to Top Start -->
+    <a href="#" class="scroll-to-top"><i class="fa fa-long-arrow-up"></i></a>
+    <!-- Back to Top End -->
+    
+    
+    <!-- All Javascript Plugins  -->
+    <script type="text/javascript" src="js/jquery.min.js"></script>
+    <script type="text/javascript" src="js/plugin.js"></script>
+    
+    <!-- Main Javascript File  -->
+    <script type="text/javascript" src="js/scripts.js"></script>
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+	  
+	
+      <!-- end copyrights -->
+      <a href="#home" data-scroll class="dmtop global-radius"><i class="fa fa-angle-up"></i></a>
+      <!-- all js files -->
+      <script src="js/all.js"></script>
+      <!-- all plugins -->
+      <script src="js/custom.js"></script>
+      <!-- map -->
+     <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCNUPWkb4Cjd7Wxo-T4uoUldFjoiUA1fJc&callback=myMap"></script>
+   </body>
+</html>
